54286cc38287274a06be0b693d541b9d8497020e
Wasin Jamroensap <wasinjamroensap@ip-10-254-0-72.ap-southeast-1.compute.internal>
2023-03-14 12:46:49 +0700

    Update Log Request

--- index.js
+++ index.js
@@ -6,17 +6,51 @@ const port = 3000
 
 const jsonParser = bodyParser.json()
 
+const logRequest = ({ method, originalUrl, query, body, param }) => {
+  console.log(`======================== [${method}] ${originalUrl} ========================`)
+  const log = {
+    method,
+    originalUrl,
+    query,
+    body,
+    param
+  }
+
+  console.log(JSON.stringify(log))
+}
+
 app.get('', jsonParser,  (req, res) => {
-  console.log('---- Start GET ----')
-  console.log(req.body)
-  console.log('---- End GET ----')
+  logRequest(req)
   res.send('Hello GET')
 })
 
 app.post('', jsonParser, (req, res) => {
-  console.log('---- Start POST ----')
-  console.log(req.body)
-  console.log('---- End POST ----')
+  logRequest(req)
+  res.send('Hello POST')
+})
+
+app.post('/create-order', jsonParser, (req, res) => {
+  logRequest(req)
+  res.send('Hello POST')
+})
+
+app.post('/multi-ready-to-fulfillment', jsonParser, (req, res) => {
+  logRequest(req)
+  res.send('Hello POST')
+})
+
+app.post('/ship-order-fulfillment', jsonParser, (req, res) => {
+  logRequest(req)
+  res.send('Hello POST')
+})
+
+app.post('/create-product', jsonParser, (req, res) => {
+  logRequest(req)
+  res.send('Hello POST')
+})
+
+app.post('/put-product', jsonParser, (req, res) => {
+  logRequest(req)
   res.send('Hello POST')
 })
 
